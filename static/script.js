6108feb53ce85967529613ba1e1212f60dbb3e84
Drew Jordan <ajordan12@luc.edu>
2023-05-29 18:50:00 -0500

    updates

---
+++ static/script.js
@@ -0,0 +1,110 @@
+var relayState = {
+    18: false,
+    23: false,
+    24: false,
+    25: false
+};
+
+function setRelay(channel, state) {
+    relayState[channel] = (state === 'on');
+    console.log('Setting relay', channel, 'to', state);
+    fetch('/set-relay', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            channel: channel,
+            state: state
+        })
+    })
+
+}
+
+
+// Fetch sensor data and update display elements
+function updateSensorData() {
+  fetch('/static/data.json')
+      .then(response => response.json())
+      .then(data => {
+          document.getElementById('temperature').innerHTML = data.temperature.toFixed(1);
+          temperatureGauge.setValueAnimated(data.temperature);
+
+          document.getElementById('humidity').innerHTML = data.humidity.toFixed(1);
+          humidityGauge.setValueAnimated(data.humidity);
+
+          document.getElementById('moisture').innerHTML = data.moisture.toFixed(1);
+          moistureGauge.setValueAnimated(data.moisture);
+      })
+      .catch(error => console.error(error));
+}
+
+
+// Update gauge value when sensor data is fetched
+function updateGaugeValue(value, type) {
+    switch(type) {
+        case 'temperature':
+            temperatureGauge.setValueAnimated(value);
+            break;
+        case 'humidity':
+            humidityGauge.setValueAnimated(value);
+            break;
+        case 'moisture':
+            moistureGauge.setValueAnimated(value);
+            break;
+        default:
+            break;
+    }
+}
+
+// Create the gauges instances
+var temperatureGauge, humidityGauge, moistureGauge;
+document.addEventListener("DOMContentLoaded", function() {
+    temperatureGauge = Gauge(
+        document.getElementById("gauge-temperature"),
+        {
+            max: 150,
+            value: 0,
+            label: 'Temperature (°C)',
+            color: '#FF7F50'
+        }
+    );
+
+    humidityGauge = Gauge(
+        document.getElementById("gauge-humidity"),
+        {
+            max: 100,
+            value: 0,
+            label: 'Humidity (%)',
+            color: '#00BFFF'
+        }
+    );
+
+    moistureGauge = Gauge(
+        document.getElementById("gauge-moisture"),
+        {
+            max: 2000,
+            value: 0,
+            label: 'Moisture',
+            color: '#3CB371'
+        }
+    );
+});
+
+// Set up event listeners for relay switches
+document.querySelectorAll('.relay-control input[type="checkbox"]').forEach(function(checkbox) {
+    checkbox.addEventListener('change', function(event) {
+        var channel = parseInt(event.target.closest('.relay-control').id.replace('relay-', ''));
+        var state = event.target.checked ? 'on' : 'off';
+        setRelay(channel, state);
+    });
+});
+
+// Fetch initial sensor data and update gauges
+updateSensorData();
+
+// Periodically update sensor data and gauges
+setInterval(updateSensorData, 5000);
+
+
+
