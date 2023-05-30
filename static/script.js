cf06a0298ac3f2217fe7cf9f8b2dbc8e79babc44
Drew Jordan <ajordan12@luc.edu>
2023-05-29 19:36:03 -0500

    page updates

--- static/script.js
+++ static/script.js
@@ -18,27 +18,46 @@ function setRelay(channel, state) {
             state: state
         })
     })
-
 }
-
 
 // Fetch sensor data and update display elements
 function updateSensorData() {
-  fetch('/static/data.json')
-      .then(response => response.json())
-      .then(data => {
-          document.getElementById('temperature').innerHTML = data.temperature.toFixed(1);
-          temperatureGauge.setValueAnimated(data.temperature);
+    fetch('/static/data.json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('temperature').innerHTML = data.temperature.toFixed(1);
+            temperatureGauge.setValueAnimated(data.temperature);
 
-          document.getElementById('humidity').innerHTML = data.humidity.toFixed(1);
-          humidityGauge.setValueAnimated(data.humidity);
+            document.getElementById('humidity').innerHTML = data.humidity.toFixed(1);
+            humidityGauge.setValueAnimated(data.humidity);
 
-          document.getElementById('moisture').innerHTML = data.moisture.toFixed(1);
-          moistureGauge.setValueAnimated(data.moisture);
-      })
-      .catch(error => console.error(error));
+            document.getElementById('moisture').innerHTML = data.moisture.toFixed(1);
+            moistureGauge.setValueAnimated(data.moisture);
+        })
+        .catch(error => console.error(error));
 }
 
+// Fetch plant care advice and update display elements
+function updatePlantCareAdvice() {
+    fetch('/plant-care-advice')
+        .then(response => response.json())
+        .then(data => {
+            // Update plant care advice section here
+            console.log(data);
+        })
+        .catch(error => console.error(error));
+}
+
+// Fetch plant health data and update display elements
+function updatePlantHealthData() {
+    fetch('/plant-health-data')
+        .then(response => response.json())
+        .then(data => {
+            // Update plant health monitoring section here
+            console.log(data);
+        })
+        .catch(error => console.error(error));
+}
 
 // Update gauge value when sensor data is fetched
 function updateGaugeValue(value, type) {
@@ -59,6 +78,7 @@ function updateGaugeValue(value, type) {
 
 // Create the gauges instances
 var temperatureGauge, humidityGauge, moistureGauge;
+
 document.addEventListener("DOMContentLoaded", function() {
     temperatureGauge = Gauge(
         document.getElementById("gauge-temperature"),
@@ -89,6 +109,11 @@ document.addEventListener("DOMContentLoaded", function() {
             color: '#3CB371'
         }
     );
+
+    // Fetch initial sensor data, plant care advice and health data
+    updateSensorData();
+    updatePlantCareAdvice();
+    updatePlantHealthData();
 });
 
 // Set up event listeners for relay switches
@@ -100,11 +125,8 @@ document.querySelectorAll('.relay-control input[type="checkbox"]').forEach(funct
     });
 });
 
-// Fetch initial sensor data and update gauges
-updateSensorData();
 
-// Periodically update sensor data and gauges
+// Periodically update sensor data, plant care advice and health data
 setInterval(updateSensorData, 5000);
-
-
-
+setInterval(updatePlantCareAdvice, 5000);
+setInterval(updatePlantHealthData, 5000);
